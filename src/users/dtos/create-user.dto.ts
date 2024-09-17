ea83eb3ed78c58298ebff65fc298ea788c875b4d
Krystian Tyśkiewicz <krystian.tyskiewicz@gmail.com>
2024-09-17 15:49:02 +0200

    Validation

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,6 +1,19 @@
+import { IsDefined, IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsDefined()
+  @IsEmail()
   email: string;
+
+  @IsDefined()
+  @IsNotEmpty()
   firstName: string;
+
+  @IsDefined()
+  @IsNotEmpty()
   lastName: string;
+
+  @IsDefined()
+  @IsNotEmpty()
   password: string;
 }
